2e8abc84995cc0524e2a6b117f973b74931bdb2c
AndreyBaglai <andreybaglai@gmail.com>
2021-09-20 16:11:09 +0300

    refactor: footer component

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
-import { Layout } from 'antd'
+import { Layout, Typography } from 'antd'
 
 import styles from './styles.module.scss'
 
-const Footer = () => <Layout.Footer className={styles.footer}>Application template footer</Layout.Footer>
+const Footer = () => (
+  <Layout.Footer className={styles.footer}>
+    <Typography.Text>Application template footer &copy;</Typography.Text>
+  </Layout.Footer>
+)
 
 export default Footer
